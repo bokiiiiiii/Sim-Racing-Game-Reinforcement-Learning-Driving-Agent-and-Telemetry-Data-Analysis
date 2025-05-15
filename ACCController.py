c237d7b54267636937fd611dfc6d5faa5d785986
bokiiiiiii <r10522605@ntu.edu.tw>
2025-05-16 01:57:24 +0800

    Revise vJoy command normalization scale

--- ACCController.py
+++ ACCController.py
@@ -8,18 +8,18 @@ else:
     print("ACC Controller initialization failed.")
 
 
-def set_steering(value):  # -100 <= value <= 100
-    steering_value = int((value + 100) / 200 * 32768)
+def set_steering(value):  # -1 <= value <= 1
+    steering_value = int((value + 1) / 2 * 32768)
     joystick.set_axis(pyvjoy.HID_USAGE_X, steering_value)
 
 
-def set_throttle(value):  # 0 <= value <= 100
-    throttle_value = int(value / 100 * 32768)
+def set_throttle(value):  # 0 <= value <= 1
+    throttle_value = int(value * 32768)
     joystick.set_axis(pyvjoy.HID_USAGE_SL1, throttle_value)
 
 
-def set_brake(value):  # 0 <= value <= 100
-    brake_value = int(value / 100 * 32768)
+def set_brake(value):  # 0 <= value <= 1
+    brake_value = int(value * 32768)
     joystick.set_axis(pyvjoy.HID_USAGE_SL0, brake_value)
 
 
@@ -27,3 +27,4 @@ def reset():
     set_steering(0)
     set_throttle(0)
     set_brake(0)
+    joystick.reset()
